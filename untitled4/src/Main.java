870d177a50926a419ef6c056a57939e0d3ee5807
stovra <125475901+stovra@users.noreply.github.com>
2023-02-22 18:54:59 +0100

    Update Main.java

--- untitled4/src/Main.java
+++ untitled4/src/Main.java
@@ -19,17 +19,17 @@ public class Main {
 
         if (thirdCar.equals("no")) {
             System.out.println("Favourite car " + cars.get(0) + ", second favourite " + cars.get(1));
-        } else cars.add(thirdCar);
+        } else { cars.add(thirdCar);
         System.out.println("Favourite car " + cars.get(0) + ", second favourite " + cars.get(1));
-        System.out.println("And the car you hate is " + cars.get(2));
+        System.out.println("And the car you hate is " + cars.get(2)); }
 
 
     }
-     // Fol neka anketa za 10 ljudi..
+     // 
     public static void main(String[] args) {
-        for (int i = 0; i<10; i++){
+       
         myFunction();
-        }
+       
     }
 
 }
